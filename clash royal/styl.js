292f984e486dabb3be6062afbbcc566e0f0869d7
xandervdberg <xavandebergen@gmail.com>
2018-09-21 17:22:32 +0200

    l

--- clash royal/styl.js
+++ clash royal/styl.js
@@ -17,10 +17,7 @@ botton4 = document.querySelector("#vierde")
 botton4.addEventListener("mouseover", function () {
     botton4.style.color = "blue"
 })
-botton5 = document.querySelector("#vijfde")
-botton5.addEventListener("mouseover", function () {
-    botton5.style.color = "blue"
-})
+
 botton.addEventListener("mouseout", function () {
     botton.style.color = "greenyellow"
 })
@@ -32,7 +29,4 @@ botton3.addEventListener("mouseout", function () {
 })
 botton4.addEventListener("mouseout", function () {
     botton4.style.color = "greenyellow"
-})
-botton5.addEventListener("mouseout", function () {
-    botton5.style.color = "greenyellow"
 })
